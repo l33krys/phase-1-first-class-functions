5f35318bedb5c28acb83238bdc0533888c7f696f
Krystle Lee <l33.krys@gmail.com>
2023-08-03 09:21:40 -0700

    complete lab advanced functions

--- index.js
+++ index.js
@@ -9,3 +9,42 @@ function returnsANamedFunction() {
 function returnsAnAnonymousFunction() {
     return () => console.log(`hello`);
 }
+
+const returnFirstTwoDrivers = function(arr) {
+    let newArr = arr.splice(0,2);
+    console.log(newArr);
+}
+
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+    
+const returnLastTwoDrivers = function(arr) {
+    let newArr = arr.splice(-2);
+    console.log(newArr);
+}
+
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+function selectingDrivers(arr1, arr2) {
+    return returnFirstTwoDrivers(arr1);
+    return returnLastTwoDrivers(arr2);
+}
+
+function createFareMultiplier(multi) {
+    // rewrite as arrow function
+    // return fare => console.log(fare * multi);
+    return function(fare) {
+        console.log(fare * multi);
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arg1, func) {
+    return func(arg1);
+}
+
+selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers);
+
+selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers);
